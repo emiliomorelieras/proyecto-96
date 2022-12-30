3e89fe0ed801af6d2c2c6bb353034a3be6a7a825
emiliomorelieras <112988503+emiliomorelieras@users.noreply.github.com>
2022-12-30 12:44:46 -0600

    Add files via upload

---
+++ carpeta sin título 4/main2.js
@@ -0,0 +1,8 @@
+player1_name=localStorage.getItem("player1_name");
+player2_name=localStorage.getItem("player2_name");
+player1_score= 1;
+player2_score= 1;
+document.getElementById("playe1_name").innerHTML=player1_name+"=";
+document.getElementById("player2_name").innerHTML=player2_name+"=";
+document.getElementById("playe1_score").innerHTML=player1_score+"=";
+document.getElementById("playe2_score").innerHTML=player2_score+"=";
